19d3c02ff481773e45a8d155adc6eda7759bd100
Oleksandr Hertsan <alexgercan88@gmail.com>
2022-10-15 18:23:21 +0300

    homework

---
+++ src/Homework8.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Homework8 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+
+        if (number == 0) {
+            System.out.println("Ошибок не обнаружено");
+        } else {
+            System.out.println("ERROR");
+            System.out.println("ERROR");
+            System.out.println("ERROR");
+
+        }
+    }
+}
