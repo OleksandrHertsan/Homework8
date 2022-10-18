5d4b5428b21f6022cc1c3b4181d4f716a07cbe4f
Oleksandr Hertsan <alexgercan88@gmail.com>
2022-10-18 17:53:10 +0300

    homework9.1

---
+++ src/homework9_1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class homework9_1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner (System.in);
+
+        int allTheWay = scanner.nextInt();
+        int travelTime = scanner.nextInt();
+        int kmPerHours = scanner.nextInt();
+
+        if ((travelTime * kmPerHours) > allTheWay / 2){
+            System.out.println("Джон ближе к Мери");
+
+        }
+        else {
+            System.out.println("Джон ближе к Шерлоку");
+        }
+
+
+    }
+}
