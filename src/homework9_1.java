e0ea94e8796edd8e640ae71c878134992526367f
Oleksandr Hertsan <alexgercan88@gmail.com>
2022-10-21 11:17:51 +0300

    homework10

--- src/homework9_1.java
+++ src/homework9_1.java
@@ -3,19 +3,15 @@ import java.util.Scanner;
 public class homework9_1 {
     public static void main(String[] args) {
 
+        int airTemperature;
+
+
         Scanner scanner = new Scanner (System.in);
 
-        int allTheWay = scanner.nextInt();
-        int travelTime = scanner.nextInt();
-        int kmPerHours = scanner.nextInt();
-
-        if ((travelTime * kmPerHours) > allTheWay / 2){
-            System.out.println("Джон ближе к Мери");
-
-        }
-        else {
-            System.out.println("Джон ближе к Шерлоку");
-        }
+        do{
+            airTemperature = scanner.nextInt();
+        }while (airTemperature >= -15);
+        System.out.println("Разгерметизация, пилоты погибли");
 
 
     }
---
+++ src/org/wikipedia/Homework10.java
@@ -0,0 +1,15 @@
+package org.wikipedia;
+
+public class Homework10 {
+    public static void main(String[] args) {
+
+        int a = 0;
+        String s = "Я начинаю разбираться с циклами";
+        while (a < 3){
+            a++;
+            System.out.println(s);
+        }
+
+
+    }
+}
