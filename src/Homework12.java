3b92c4ab619fab6aff070c9c9ade27c6909b67a0
Oleksandr Hertsan <alexgercan88@gmail.com>
2022-10-26 21:37:31 +0300

    homework12

---
+++ src/Homework12.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Homework12 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number % 2 == 0){
+            System.out.println(2);
+        }
+        else {
+            System.out.println(number);
+        }
+
+
+
+
+
+    }
+}
