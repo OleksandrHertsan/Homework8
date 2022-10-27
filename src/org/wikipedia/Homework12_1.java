2ffec0688628022c6e1f7662943bd02c544458dc
Oleksandr Hertsan <alexgercan88@gmail.com>
2022-10-27 21:45:19 +0300

    homework12_1

---
+++ src/org/wikipedia/Homework12_1.java
@@ -0,0 +1,23 @@
+package org.wikipedia;
+
+import java.util.Scanner;
+
+public class Homework12_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int money = scanner.nextInt();
+        int percent = scanner.nextInt();
+        int years = scanner.nextInt();
+        for (int i = 0; i < years ; i++) {
+            money = money + (money * percent / 100);
+                System.out.println(money);
+            }
+
+        }
+
+
+
+
+
+    }
+
