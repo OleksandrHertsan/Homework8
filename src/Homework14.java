916353fd10a704b8caefb10f3be7c0c7aef4271f
Oleksandr Hertsan <alexgercan88@gmail.com>
2022-10-28 17:43:26 +0300

    Homework14

---
+++ src/Homework14.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class Homework14 {
+
+
+
+    public static void main(String[] args) {
+
+    int[] massive = new int[100];
+
+        for (int i = 1; i < 99; i++) {
+            massive[i] = 10;
+            massive[0] = 0;
+            massive[99] = 0;
+
+        }
+
+
+
+
+
+        System.out.println(Arrays.toString(massive));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
